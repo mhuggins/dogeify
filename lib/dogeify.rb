5f1b0f7c687d9f5c0ccca2a4fb17719ce2ac9271
Matt Huggins <matt.huggins@gmail.com>
2014-03-31 11:03:20 -0600

    Add more emotional summaries, and include intentional misspellings

--- lib/dogeify.rb
+++ lib/dogeify.rb
@@ -2,26 +2,65 @@ require 'dogeify/version'
 require 'engtagger'
 
 class Dogeify
-  ADJECTIVES = %w(so such very much many).freeze
+  ADJECTIVES = %w[so such very much many how].freeze
+  EMOTIONS = %w[wow amaze excite].freeze
 
   def initialize
     @tagger = EngTagger.new
   end
 
   def process(str)
-    tagged_str = tagger.add_tags(str.downcase)
-    phrases = tagger.get_nouns(tagged_str).keys
+    # Parse sentences.
+    sentences = str.downcase.split(/[\.!?]+/).map(&:strip)
 
-    phrases = phrases.each_with_index.map { |phrase, i| "#{adjective(i)} #{phrase}." }
-    phrases << 'wow.'
-    phrases.join(' ')
+    sentences = sentences.map do |sentence|
+      # Select just the nouns.
+      tagged_sentence = tagger.add_tags(sentence)
+      phrases = tagger.get_nouns(tagged_sentence).keys rescue []
+
+      # Prefix nouns with adjectives, and convert to sentences.
+      phrases.map! { |phrase| correct_spelling(phrase) }
+      phrases.map! { |phrase| "#{adjective} #{phrase}." }
+
+      # Append a word or phrase describing emotion.
+      phrases << "#{emotional_summary}."
+
+      phrases.join(' ')
+    end
+
+    sentences.join(' ')
   end
 
   private
 
   attr_reader :tagger
 
-  def adjective(i)
-    ADJECTIVES[i % ADJECTIVES.size]
+  def correct_spelling(word)
+    word.dup.tap do |word|
+      word.gsub!(/er$/, 'ar')                    # super => supar
+      word.gsub!(/ph/, 'f')                      # phone => fone
+      word.gsub!(/cious/, 'shus')                # delicious => delishus, deliciousness => delishusness
+      word.gsub!(/([^s])tion(s$)?/, '\1shun\2')  # emotion => emoshun, emotions => emoshuns, emotionless => emoshunless, question (unchanged)
+      word.gsub!(/stion$/, 'schun')              # question => queschun, potion (unchanged)
+      word.gsub!(/dog([^e]|\b)/, 'doge\1')       # dog => doge, dogs => doges, underdog => underdoge, doge (unchanged)
+    end
+  end
+
+  def adjective
+    ADJECTIVES[adjective_offset]
+  end
+
+  def adjective_offset
+    @adjective_offset ||= -1
+    @adjective_offset = (@adjective_offset + 1) % ADJECTIVES.size
+  end
+
+  def emotional_summary
+    EMOTIONS[emotional_summary_offset]
+  end
+
+  def emotional_summary_offset
+    @emotional_summary_offset ||= -1
+    @emotional_summary_offset = (@emotional_summary_offset + 1) % EMOTIONS.size
   end
 end
--- spec/dogeify_spec.rb
+++ spec/dogeify_spec.rb
@@ -1,10 +1,21 @@
 require 'spec_helper'
 
 describe Dogeify do
-  subject { Dogeify.new }
+  subject { described_class.new }
 
   describe '#process' do
-    let(:input) { 'My grandmom gave me a sweater for Christmas.' }
+    let(:input) do
+      %q{Doge is a slang term for "dog" that is primarily associated with
+         pictures of Shiba Inus (nicknamed "Shibe") and internal monologue
+         captions on Tumblr. These photos may be photoshopped to change the
+         dog's face or captioned with interior monologues in Comic Sans font.
+         The use of the misspelled word "doge" to refer to a dog dates back
+         to June 24th, 2005, when it was mentioned in an episode of Homestar
+         Runner's puppet show. In the episode titled "Biz Cas Fri 1",
+         Homestar calls Strong Bad his "d-o-g-e" while trying to distract him
+         from his work.}
+    end
+
     let(:output) { subject.process(input) }
 
     it 'converts to lowercase' do
@@ -12,13 +23,37 @@ describe Dogeify do
     end
 
     it 'combines nouns with doge adjectives' do
-      expect(output).to match /so grandmom\./i
-      expect(output).to match /such sweater\./i
-      expect(output).to match /very christmas\./i
+      expected = 'so doge. such slang. very term. much doge. many pictures. ' <<
+                 'how shiba. so inus. such shibe. very monologue. much ' <<
+                 'capshuns. many tumblr.'
+      expect(output).to start_with expected
     end
 
-    it 'always appends "wow."' do
-      expect(output).to end_with 'wow.'
+    it 'appends an emotional summary to the end of each sentence' do
+      expect(output).to match /(wow\.).*(amaze\.).*(excite\.).*(wow\.)/
+    end
+
+    describe 'when the input is an empty string' do
+      let(:input) { '' }
+
+      it 'does not raise an error' do
+        expect { output }.to_not raise_error
+      end
+    end
+
+    describe 'when the input contains misspellable words' do
+      let(:input) do
+        %q{Mister Phoebe is always so emotionless. Maybe he needs a dog to
+           cheer him up. It might be a question worth answering!}
+      end
+
+      it 'intentionally misspells words' do
+        expect(output).to include 'mistar'
+        expect(output).to include 'foebe'
+        expect(output).to include 'emoshunless'
+        expect(output).to include 'doge'
+        expect(output).to include 'queschun'
+      end
     end
   end
 end
